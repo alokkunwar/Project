0af1ce4fafc5ca22c193d0110fad459feae1076c
Ankit kunwar <ankitkunwar.101@gmail.com>
2022-06-10 18:49:15 +0530

    First

---
+++ src/component/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="content">
+          <h3>Fresh Water</h3>
+          <span>Natural Drinking Water</span>
+          <p>
+            Thirst is your body's way of telling you that it's running low on
+            water. Feeling thirsty? Here you go on Drop, we offer you our
+            wide range of beverages water. We care about our customers and so as
+            for their needs and choices too. We provide you freshly packaged
+            drinking water anywhere, anytime. Drinking pure and healthy water
+            will prevent you from headaches, cramps, sprains etc. It helps to
+            provide energy and increases brain power, promotes healthy weight
+            management, boosts the immune system and what not! It is the basic
+            necessity of life to survive to be alive!
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/component/signup.component.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+export default class SignUp extends Component {
+  render() {
+    return (
+      <div className="card container" style={{ width: "30%" ,marginTop:"75px",borderRadius:"10px",padding: "40px 55px 45px 55px"}}>
+      <form>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" />
+        </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label style={{}}>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered <a href="/sign-in">Sign In?</a>
+        </p>
+      </form>
+       </div>
+    )
+  }
+}
